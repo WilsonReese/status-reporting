c5b17a4855e44bbd91f41973379f0b0681d0ad0f
WilsonReese <reese@chicagobooth.edu>
2021-05-17 18:28:42 +0000

    added devise user scaffold

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  bench_status           :integer          default(0)
+#  user_type              :integer          default(1)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  bench_status           :integer          default(0)
+#  user_type              :integer          default(1)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
